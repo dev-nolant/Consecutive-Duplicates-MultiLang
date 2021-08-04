0645813a03cb1c46cee5c580594127446e8e86a1
Nolan Taft <killer123yo@gmail.com>
2021-08-03 21:58:27 -0600

    Update remove_dupe.cpp

--- remove_dupe.cpp
+++ remove_dupe.cpp
@@ -1,9 +1,7 @@
 // Remove immediate duplicates in a provided string
 
 string solve(string s) { // Declairs a function solve, which accepts the variable 's' and turns it into a string form.
-    auto last = std::unique(s.begin(), s.end()); // auto declaires the variable 'last' automatically depending on what type of data is written to it.
-                                                // std::unique removes all duplicate chars in the string from the beginning(s.begin()) until the end(s.end())
-    s.erase(last, s.end()); // erases the end of the string that is not able to be processed by the unique function above.
+    s.erase(unique(s.begin(), s.end()), s.end()); // std::unique removes all duplicate chars in the string from the beginning(s.begin()) until the end(s.end())
     return s; // 'nuff said
 }
 
